a1a788c9392cc39cd947258932fd62e1a6bcbfcf
vvvvesper <35946769+vvvvesper@users.noreply.github.com>
2022-08-16 07:32:48 +0800

    2-8 SpringBoot自定义配置项

--- src/main/java/com/imooc/wiki/controller/TestController.java
+++ src/main/java/com/imooc/wiki/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.imooc.wiki.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${test.name}")
+    String testName;
+
     @GetMapping("/hello")
     public String test(){
-        return "hello world!";
+        return "hello world!" + testName;
     }
 }
