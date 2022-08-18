f7f37ffb62f5e01166796f19ef0f327c3a3f5422
vvvvesper <35946769+vvvvesper@users.noreply.github.com>
2022-08-18 08:26:05 +0800

    3-8 封装CopyUtil

--- src/main/java/com/imooc/wiki/service/EbookService.java
+++ src/main/java/com/imooc/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.imooc.wiki.domain.Ebook;
 import com.imooc.wiki.domain.EbookExample;
 import com.imooc.wiki.mapper.EbookMapper;
 import com.imooc.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.imooc.wiki.util.CopyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +23,15 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> ebookRespList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook,ebookResp);
-            ebookRespList.add(ebookResp);
-        }
+//        List<EbookResp> ebookRespList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+//            // EbookResp ebookResp = new EbookResp();
+//            // BeanUtils.copyProperties(ebook,ebookResp);
+//            // 单体复制
+//            EbookResp ebookResp = CopyUtil.copy(ebook,EbookResp.class);
+//            ebookRespList.add(ebookResp);
+//        }
+        List<EbookResp> ebookRespList = CopyUtil.copyList(ebookList,EbookResp.class);
         return ebookRespList;
     }
 }
