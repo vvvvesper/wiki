2f1375956d937921122af974fc1613995fd3bb18
vvvvesper <35946769+vvvvesper@users.noreply.github.com>
2022-08-19 08:39:39 +0800

    4-5 集成Ant Design Vue

--- web/src/views/HomeView.vue
+++ web/src/views/HomeView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="home">
+    <a-button danger>Danger</a-button>
+    <a-button type="dashed">Dashed</a-button>
+
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
